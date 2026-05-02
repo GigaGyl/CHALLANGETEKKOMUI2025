005d590570b4428154979ca0f4b80971f01d1977
GigaGyl <gilchristweru@gmail.com>
2026-05-02 12:12:45 +0700

    Second Commit

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -11,4 +11,14 @@ int main() {
 
     printf("From: Gyl\n");
     printf("Semangat seleksi calon aslab 2025! (terima gw plis)\n");
+    printf("\n");
+    printf("Semangat buat diriku yg di masa depan\n");
+    printf("You've been through so much\n");
+    printf("Jangan lupa mulaimu dari mana dan semua orang yang telah bantu dalam perjalananmu\n");
+    printf("Wherever you are in life now, I know you've done your best and will keep doing your best\n");
+    printf("\n");
+    printf("Remember that your past doesn't chain you\n");
+    printf("To regret means you are someone wouldn't do the same thing now\n");
+    printf("You are not me, you're better\n");
+    printf("Best of luck, be grateful, and be blessed!\n");
 }
